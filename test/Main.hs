4fab2030c838bde880293341fc164a422d1ec2c1
Tom McLaughlin <tom@codedown.io>
2023-06-05 03:28:30 -0700

    Fix up tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,31 +1,11 @@
 
 import Test.Sandwich
 
-import qualified Test.BinarySearchVec
-
-import qualified Test.Transformer.DirectiveToPragma
-import qualified Test.Transformer.ExpressionToDeclaration
-import qualified Test.Transformer.StatementToDeclaration
-import qualified Test.Transformer.ImportSifter
-import qualified Test.Transformer.PragmaSifter
-import qualified Test.Transformer.StripDirective
-
 import qualified Test.Hover
 
 
 spec :: TopSpec
 spec = do
-  describe "Util" $ do
-    Test.BinarySearchVec.spec
-
-  describe "Transformers" $ do
-    Test.Transformer.DirectiveToPragma.spec
-    Test.Transformer.ExpressionToDeclaration.spec
-    Test.Transformer.StatementToDeclaration.spec
-    Test.Transformer.ImportSifter.spec
-    Test.Transformer.PragmaSifter.spec
-    Test.Transformer.StripDirective.spec
-
   Test.Hover.spec
 
 main :: IO ()
--- test/Test/Hover.hs
+++ test/Test/Hover.hs
@@ -16,17 +16,17 @@ spec = describe "Hover" $ do
 
     it "Basic case" $ do
       fixupDocumentReferences' (mkDocRegex "main.ipynb") transformer "Defined at: main.ipynb:3:1"
-        >>= (`shouldBe` "Defined at: main.ipynb:1:1")
+        >>= (`shouldBe` "Defined at: main.ipynb:2:1")
 
     it "Works with a long path with spaces and stuff" $ do
       let longPath = "/home/tom/tools/codedown-languages/tests/test_runs/2023-04-04T22_19_24.564991588Z/results/0_command line options/Introduce parallel semaphore/Tests/Parallel/claim semaphore/Haskell haskell-ghc8107/Haskell Nix environment/Jupyter runner/LSP/Hover/0_hovers foo/haskell-language-server-fc572b32aa084825/main.ipynb.hs"
       let regex = mkDocRegex longPath
       fixupDocumentReferences' regex transformer [i|Defined at: #{longPath}:3:1|]
-        >>= (`shouldBe` [i|Defined at: #{longPath}:1:1|])
+        >>= (`shouldBe` [i|Defined at: #{longPath}:2:1|])
 
     it "Works with weird PCRE characters in the file name" $ do
       fixupDocumentReferences' (mkDocRegex "ma$in.i?pynb") transformer "Defined at: ma$in.i?pynb:3:1"
-        >>= (`shouldBe` "Defined at: ma$in.i?pynb:1:1")
+        >>= (`shouldBe` "Defined at: ma$in.i?pynb:2:1")
 
 hoverLines = [
   "foo = 42"
